0daabba6ef9c59dbed7fe1944a8d12ae4c512f11
Christopher-Teague <cmteague77@gmail.com>
2021-10-30 10:23:27 -0700

    need to add button functionality

--- server.py
+++ server.py
@@ -5,16 +5,24 @@ app.secret_key = 'BoomBaby'
 @app.route('/')
 def visit():
     session['visits'] += int(1) 
-    
     if 'visits' in session:
         print('key exists!')
     else:
         print("key 'visits' does NOT exist")
-    
     return render_template('index.html')
 
-    session['visits'] = request.form['visits']
+@app.route('/destroy_session')
+def reset():
+    session['visits'] = int(0)
+    return redirect('/')
+    
+@app.route('/counter.html')
+def counter():
+    session['visits'] += int(2)
+    return render_template('counter.html')
 
+
+    session['visits'] = request.form['visits']
 
 
 if __name__=="__main__":
