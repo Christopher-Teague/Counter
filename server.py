4fb0ce1ffeeb83e8abe073903f990e7cb2cf9725
Christopher-Teague <cmteague77@gmail.com>
2021-10-29 23:30:16 -0700

    origin

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'BoomBaby'
+
+@app.route('/')
+def visit():
+    session['visits'] += int("1") 
+    
+    if 'visits' in session:
+        print('key exists!')
+    else:
+        print("key 'visits' does NOT exist")
+    
+    return render_template('index.html')
+
+    session['visits'] = request.form['visits']
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
